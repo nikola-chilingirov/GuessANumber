30190f24b9a43f607aaa9d9d034bf14ccb878e30
nikola-chilingirov <n.chilingirovz@gmail.com>
2024-09-24 17:31:13 +0300

    Next level

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,17 +1,44 @@
 import random
 from colorama import Fore, Back, Style
-target = random.randint(1, 100)
-while True:
-    player_input = input(Fore.YELLOW + "Guess the number (1-100): ")
-    if not player_input.isdigit():
-        print("Invalid input. Try again...")
-        continue
+flag = True
+while flag:
+    counter = 0
+    next_level = False
+    end = 100
+    level = random.randint(1, end)
+    range_lev = "1-100"
+    while counter < 8:
+        player_input = input(Fore.YELLOW + f"Guess the number with {8 - counter} attempts {range_lev}:")
+        if not player_input.isdigit():
+            print("Invalid input. Try again...")
+            continue
+        else:
+            player_input_int = int(player_input)
+        counter += 1
+        if counter == 8 and player_input_int != level:
+            print(f"You can`t guess the number with {counter} attempts")
+            break
+        if player_input_int == level:
+            print(f"You guess with {counter} attempts!")
+            next_level = True
+            break
+        elif player_input_int > level:
+            print("Too High!")
+        else:
+            print("Too Low!")
+    if next_level:
+        counter = 0
+        end += 100
+        range_lev = "1-200"
+        player_input = input(Fore.YELLOW + f"Next level or Exit: [Y] or [N]")
+        if player_input == "Y":
+            continue
+        #else:
+            #break
     else:
-        player_input_int = int(player_input)
-    if player_input_int == target:
-        print("You guess it!")
-        break
-    elif player_input_int > target:
-        print("Too High!")
-    else:
-        print("Too Low!")
+        restart = input("Play again [Y] or [N]: ")
+        if restart == "Y":
+            counter = 0
+            flag = True
+        else:
+            break
